f059a8e7a3f32d81ad1e12c6647ae3b5cb8715d6
Jeffery Xie <jeffxie27@outlook.com>
2024-09-24 09:50:19 -0600

    Change engine node requirement.

--- api/openai.js
+++ api/openai.js
@@ -9,7 +9,7 @@ const openai = new OpenAIApi(configuration);
 
 export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*'); // Allow all origins
-  res.setHeader('Access-Control-Allow-Methods', 'POST, GET, OPTIONS');
+  res.setHeader('Access-Control-Allow-Methods', 'POST');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 
   if (req.method === 'POST') {
